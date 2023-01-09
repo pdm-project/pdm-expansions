1880d174f93779d7e8effdaff6d6e881601820fd
Frost Ming <me@frostming.com>
2023-01-09 11:13:53 +0800

    fix: store in global

--- api/index.py
+++ api/index.py
@@ -1,23 +1,20 @@
-from flask import g, request, Flask
-import random
 import os
+import random
+
+from flask import Flask, request
 
 app = Flask(__name__, static_folder=None)
 expansions_txt = os.path.join(os.path.dirname(app.root_path), "expansions.txt")
+with open(expansions_txt, "r") as f:
+    CHOICES = list(filter(None, f.read().splitlines()))
 
 
 @app.get("/api/random")
 def random_name():
-    return {"code": 0, "message": "success", "data": random.choice(g.choices)}
+    return {"code": 0, "message": "success", "data": random.choice(CHOICES)}
 
 
 @app.get("/api/sample")
 def random_sample():
-    k = min(request.args.get("k", 7), len(g.choices))
-    return {"code": 0, "message": "success", "data": random.sample(g.choices, k)}
-
-
-@app.before_first_request
-def load_expansions():
-    with open(expansions_txt, "r") as f:
-        g.choices = list(filter(None, f.read().splitlines()))
+    k = min(request.args.get("k", 7), len(CHOICES))
+    return {"code": 0, "message": "success", "data": random.sample(CHOICES, k)}
